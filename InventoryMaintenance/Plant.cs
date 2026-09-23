0e38f7dc6f5ba5804abaa886abaf6604697968a3
guptan0506 <navyag0509@gmail.com>
2026-09-23 15:55:17 -0400

    Add project files.

---
+++ InventoryMaintenance/Plant.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InventoryMaintenance
+{
+    public class Plant : InvItem, IDisplayable
+    {
+        public Plant() { }
+
+        public Plant(int itemNo, string description, decimal price, string size) :
+            base(itemNo, description, price)
+        {
+            Size = size;
+        }
+
+        public string Size { get; set; }
+
+        public string GetDisplayText()
+        {
+            return $"{ItemNo}    {Size} {Description} ({Price:c})";
+        }
+        
+        // public override string GetDisplayText() => 
+          //  $"{ItemNo}    {Size} {Description} ({Price:c})";
+    }
+}
---
+++ InventoryMaintenance/Supply.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InventoryMaintenance
+{
+    public class Supply : InvItem, IDisplayable
+    {
+        public Supply() { }
+
+        public Supply(int itemNo, string description, decimal price, string manufacturer) :
+            base(itemNo, description, price)
+        {
+            Manufacturer = manufacturer;
+        }
+
+        public string Manufacturer { get; set; }
+
+        //public override string GetDisplayText() =>
+        //    $"{ItemNo}    {Manufacturer} {Description} ({Price:c})";
+
+        public string GetDisplayText()
+        {
+            return $"{ItemNo}    {Manufacturer} {Description} ({Price:c})";
+        }
+
+    }
+}
